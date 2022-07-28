67343a574550631d9ab12645931532180d45de3a
kateyes123 <kateyes123@daum.net>
2022-07-28 23:14:08 +0900

    오류

--- src/main/java/com/kh/bookie/member/model/service/MemberService.java
+++ src/main/java/com/kh/bookie/member/model/service/MemberService.java
@@ -8,20 +8,7 @@ import com.kh.bookie.member.model.dao.MemberDao;
 import com.kh.bookie.member.model.dto.Member;
 
 public interface MemberService {
-<<<<<<< HEAD
-	String ROLE_USER = "ROLE_USER";
-	String ROLE_ADMIN = "ROLE_ADMIN";
 
-	int insertMember(Member member);
-	
-	Member selectOneMember(String memberId);
-
-	int updateMember(Member member);
-
-	List<Member> selectMemberList();
-
-	int updateMemberRole(String memberId, List<String> authorities);
-=======
 	
 	String ROLE_USER = "ROLE_USER";
 	String ROLE_ADMIN = "ROLE_ADMIN";
@@ -31,6 +18,6 @@ public interface MemberService {
 	Member selectOneMemberByNickname(String nickname);
 
 	int memberEnroll(Member member);
->>>>>>> branch 'master' of https://github.com/devcami/bookie_on_and_on.git
+
 
 }
