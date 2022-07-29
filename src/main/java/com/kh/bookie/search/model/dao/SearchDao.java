68cd43f4795d676618932636fbcf9b5f874cf7e4
devcami <juf1015@gmail.com>
2022-07-29 14:42:28 +0900

    add EmptyPrincipalCheckFilter

--- src/main/java/com/kh/bookie/search/model/dao/SearchDao.java
+++ src/main/java/com/kh/bookie/search/model/dao/SearchDao.java
@@ -1,8 +1,18 @@
 package com.kh.bookie.search.model.dao;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.kh.bookie.mypage.model.dto.Book;
 
 @Mapper
 public interface SearchDao {
+
+	int bookEnroll(Book book);
+
+	int bookIngEnroll(Book book);
+
+	Book getMyBook(Map<String, Object> map);
 
 }
--- src/main/java/com/kh/bookie/search/model/service/SearchServiceImpl.java
+++ src/main/java/com/kh/bookie/search/model/service/SearchServiceImpl.java
@@ -1,11 +1,15 @@
 package com.kh.bookie.search.model.service;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.kh.bookie.mypage.model.dto.Book;
 import com.kh.bookie.search.model.dao.SearchDao;
 
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -16,4 +20,19 @@ public class SearchServiceImpl implements SearchService {
 	@Autowired
 	private SearchDao searchDao;
 
+	@Override
+	public int bookEnroll(Book book) {
+		int result = searchDao.bookEnroll(book);
+		// status가 읽음, 읽는 중 일때만 book_ing에 추가
+		if(book.getStatus().equals("읽음") || book.getStatus().equals("읽는 중")) {
+			result = searchDao.bookIngEnroll(book);
+		}
+		// 중단, 읽고싶은, 잠시 멈춘은 추가 안해
+		return result;
+	}
+
+	@Override
+	public Book getMyBook(Map<String, Object> map) {
+		return searchDao.getMyBook(map);
+	}
 }
